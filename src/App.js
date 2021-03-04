723ba347fc7546492d52e8b4c0658b877248ec73
Arpit Trikha <arpit.trikha@gmail.com>
2021-03-04 21:36:40 +0530

    Product List Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 //feature 1 added
 import logo from './logo.svg';
-
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="grid-container">
-     <header>
-       <a href='/'>Shopping Cart</a>
-     </header>
-     <main>
-      Product List
-     </main>
-     <footer>
-       All rights are reserved.
-     </footer>
-    </div>
-  );
+import data from "./data.json"
+import Product from './components/Product';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:""
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+       <header>
+         <a href='/'>Shopping Cart</a>
+       </header>
+       <main>
+        <div className='content'>
+        <div className='main'><Product products={this.state.products}></Product></div>
+        <div className='sidebar'>Cart Items</div>
+        </div>
+       </main>
+       <footer>
+         All rights are reserved.
+       </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
