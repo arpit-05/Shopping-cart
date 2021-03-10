8067dc5beeb44281498f7838ee9f3eb21167683a
Arpit Trikha <arpit.trikha@gmail.com>
2021-03-10 16:35:18 +0530

    Create Orders and Manage Orders

---
+++ src/screens/AdminScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Orders from '../components/Orders'
+
+class AdminScreen extends Component {
+    render() {
+        return (
+           
+               <Orders></Orders>
+            
+        )
+    }
+}
+export default AdminScreen
